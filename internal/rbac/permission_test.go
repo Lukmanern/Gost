c41ff1e8a66d38ed7809e587d069c736c35e9f99
LukmanE22 <unsurlukman@gmail.com>
2023-10-20 13:41:53 +0700

    add unique-value test for roles and permissions

--- internal/rbac/permission_test.go
+++ internal/rbac/permission_test.go
@@ -11,3 +11,20 @@ func TestAllPermissions(t *testing.T) {
 		}
 	}
 }
+
+// This Test is to make sure
+// that permission value is unique
+func TestCountPermissions(t *testing.T) {
+	hashMapPermissions := make(map[string]int, 0)
+
+	for _, permission := range AllPermissions() {
+		hashMapPermissions[permission.Name] += 1
+		if hashMapPermissions[permission.Name] > 1 {
+			t.Error("should 1, not more, non-unique permission detected : ", permission.Name)
+		}
+	}
+
+	if len(hashMapPermissions) != len(AllPermissions()) {
+		t.Error("should equal len, non-unique permission detected")
+	}
+}
