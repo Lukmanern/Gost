533634f21cc9c2ef3ca64f098c987f2a3caa3164
LukmanE22 <unsurlukman@gmail.com>
2023-10-27 08:47:59 +0700

    finishing role service test

--- service/rbac/role_service_test.go
+++ service/rbac/role_service_test.go
@@ -2,9 +2,12 @@ package service
 
 import (
 	"log"
+	"strings"
 	"testing"
 
 	"github.com/Lukmanern/gost/database/connector"
+	"github.com/Lukmanern/gost/domain/base"
+	"github.com/Lukmanern/gost/domain/model"
 	"github.com/Lukmanern/gost/internal/env"
 	"github.com/Lukmanern/gost/internal/helper"
 	"github.com/Lukmanern/gost/internal/rbac"
@@ -37,11 +40,105 @@ func TestNewRoleService(t *testing.T) {
 	}
 }
 
+// create 1 role, create 4 permissions
+// trying to connect
+
 func TestSuccessCRUD_Role(t *testing.T) {
 	c := helper.NewFiberCtx()
 	ctx := c.Context()
-	svc := NewPermissionService()
-	if svc == nil || ctx == nil {
+	permSvc := NewPermissionService()
+	if permSvc == nil || ctx == nil {
 		t.Error("should not nil")
+	}
+	svc := NewRoleService(permSvc)
+	if svc == nil {
+		t.Error("should not nil")
+	}
+
+	modelRole := model.RoleCreate{
+		Name:        strings.ToLower(helper.RandomString(10)),
+		Description: helper.RandomString(30),
+	}
+	roleID, createErr := svc.Create(ctx, modelRole)
+	if createErr != nil || roleID < 1 {
+		t.Error("should not error and id should more than zero")
+	}
+
+	// save the id for delete the perms
+	permsID := make([]int, 0)
+	for i := 0; i < 3; i++ {
+		modelPerm := model.PermissionCreate{
+			Name:        strings.ToLower(helper.RandomString(10)),
+			Description: helper.RandomString(30),
+		}
+		permID, createErr := permSvc.Create(ctx, modelPerm)
+		if createErr != nil || permID < 1 {
+			t.Error("should not error and permID should more than one")
+		}
+
+		permsID = append(permsID, permID)
+	}
+
+	defer func() {
+		svc.Delete(ctx, roleID)
+		for _, id := range permsID {
+			permSvc.Delete(ctx, id)
+		}
+	}()
+
+	modelConnect := model.RoleConnectToPermissions{
+		RoleID:        roleID,
+		PermissionsID: permsID,
+	}
+	connectErr := svc.ConnectPermissions(ctx, modelConnect)
+	if connectErr != nil {
+		t.Error("should not error")
+	}
+
+	roleByID, getErr := svc.GetByID(ctx, roleID)
+	if getErr != nil || roleByID == nil {
+		t.Error("should not error and role not nil")
+	}
+	if len(roleByID.Permissions) != len(permsID) {
+		t.Error("total of permissions connected by role should equal")
+	}
+
+	roles, total, getAllErr := svc.GetAll(ctx, base.RequestGetAll{Limit: 10, Page: 1})
+	if len(roles) < 1 || total < 1 || getAllErr != nil {
+		t.Error("should more than or equal one and not error at all")
+	}
+
+	updateRoleModel := model.RoleUpdate{
+		ID:          roleID,
+		Name:        strings.ToLower(helper.RandomString(11)),
+		Description: helper.RandomString(31),
+	}
+	updateErr := svc.Update(ctx, updateRoleModel)
+	if updateErr != nil {
+		t.Error("should not error")
+	}
+
+	// value reset
+	roleByID = nil
+	getErr = nil
+	roleByID, getErr = svc.GetByID(ctx, roleID)
+	if getErr != nil || roleByID == nil {
+		t.Error("should not error and roleByID should not nil")
+	}
+	if roleByID.Name != updateRoleModel.Name || roleByID.Description != updateRoleModel.Description {
+		t.Error("name and desc should same")
+	}
+
+	deleteErr := svc.Delete(ctx, roleID)
+	if deleteErr != nil {
+		t.Error("should not error")
+	}
+
+	// value reset
+	roleByID = nil
+	getErr = nil
+	roleByID, getErr = svc.GetByID(ctx, roleID)
+	if getErr == nil || roleByID != nil {
+		t.Error("should error and roleByID should nil")
 	}
 }
