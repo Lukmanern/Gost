c019f4fb72be5d42b2a491961bf6e5c1303e9ebb
LukmanE22 <unsurlukman@gmail.com>
2023-10-29 08:52:44 +0700

    add test case for user controller -3

--- controller/user/user_controller_test.go
+++ controller/user/user_controller_test.go
@@ -211,7 +211,7 @@ func Test_Register(t *testing.T) {
 		}
 		defer resp.Body.Close()
 		if resp.StatusCode != tc.respCode {
-			t.Error("should equal", resp.StatusCode)
+			t.Error("should equal, but got", resp.StatusCode)
 		}
 
 		if tc.payload != nil {
@@ -280,31 +280,27 @@ func Test_AccountActivation(t *testing.T) {
 	testCases := []struct {
 		caseName string
 		respCode int
-		response response.Response
 		payload  *model.UserVerificationCode
 	}{
 		{
-			caseName: "success account activation",
+			caseName: "success verify",
 			respCode: http.StatusOK,
-			response: response.Response{
-				Message: "",
-				Success: true,
-				Data:    nil,
-			},
 			payload: &model.UserVerificationCode{
 				Code: *vCode,
 			},
 		},
 		{
-			caseName: "failed account activation: code not found",
+			caseName: "failed verify: code not found",
 			respCode: http.StatusNotFound,
-			response: response.Response{
-				Message: "",
-				Success: false,
-				Data:    nil,
-			},
 			payload: &model.UserVerificationCode{
 				Code: *vCode,
+			},
+		},
+		{
+			caseName: "failed verify: code too short",
+			respCode: http.StatusBadRequest,
+			payload: &model.UserVerificationCode{
+				Code: "",
 			},
 		},
 	}
@@ -332,7 +328,21 @@ func Test_AccountActivation(t *testing.T) {
 		}
 		defer resp.Body.Close()
 		if resp.StatusCode != tc.respCode {
-			t.Error("should equal", resp.StatusCode)
+			t.Error("should equal, but got", resp.StatusCode)
+		}
+
+		// if success
+		if resp.StatusCode == http.StatusOK {
+			userByEmail, getErr := userRepo.GetByEmail(ctx, createdUser.Email)
+			if getErr != nil || userByEmail == nil {
+				t.Error("should not error and user not nil")
+			}
+			if userByEmail.VerificationCode != nil {
+				t.Fatal("verif code should nil after activation")
+			}
+			if userByEmail.ActivatedAt == nil {
+				t.Fatal("activated_at should not nil after activation")
+			}
 		}
 	}
 }
@@ -346,6 +356,103 @@ func Test_DeleteAccountActivation(t *testing.T) {
 	}
 	c.Method(http.MethodPost)
 	c.Request().Header.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+	createdUser := model.UserRegister{
+		Name:     helper.RandomString(10),
+		Email:    helper.RandomEmails(1)[0],
+		Password: helper.RandomString(10),
+		RoleID:   1, // admin
+	}
+	userID, createErr := userSvc.Register(ctx, createdUser)
+	if createErr != nil || userID <= 0 {
+		t.Fatal("should success create user, user failed to create")
+	}
+	userByID, getErr := userRepo.GetByID(ctx, userID)
+	if getErr != nil || userByID == nil {
+		t.Fatal("should success get user by id")
+	}
+	vCode := userByID.VerificationCode
+	if vCode == nil || userByID.ActivatedAt != nil {
+		t.Fatal("user should inactivate for now, but its get activated/ nulling vCode")
+	}
+	defer func() {
+		userRepo.Delete(ctx, userID)
+
+		r := recover()
+		if r != nil {
+			t.Fatal("panic @ User_Test_Register ::", r)
+		}
+	}()
+
+	testCases := []struct {
+		caseName string
+		respCode int
+		payload  *model.UserVerificationCode
+	}{
+		{
+			caseName: "success delete account",
+			respCode: http.StatusOK,
+			payload: &model.UserVerificationCode{
+				Code: *vCode,
+			},
+		},
+		{
+			caseName: "failed delete account: code not found",
+			respCode: http.StatusNotFound,
+			payload: &model.UserVerificationCode{
+				Code: *vCode,
+			},
+		},
+		{
+			caseName: "failed delete account: code too short",
+			respCode: http.StatusBadRequest,
+			payload: &model.UserVerificationCode{
+				Code: "-",
+			},
+		},
+	}
+
+	endp := "/user/request-delete"
+	for _, tc := range testCases {
+		log.Println(":::::::" + tc.caseName)
+		jsonObject, err := json.Marshal(&tc.payload)
+		if err != nil {
+			t.Error("should not error", err.Error())
+		}
+		url := "http://127.0.0.1:9009" + endp
+		req, httpReqErr := http.NewRequest(http.MethodPost, url, bytes.NewReader(jsonObject))
+		if httpReqErr != nil || req == nil {
+			t.Fatal("should not nil")
+		}
+		req.Header.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+		app := fiber.New()
+		app.Post(endp, ctr.DeleteAccountActivation)
+		req.Close = true
+		resp, err := app.Test(req, -1)
+		if err != nil {
+			t.Fatal("should not error")
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != tc.respCode {
+			t.Error("should equal, but got", resp.StatusCode)
+		}
+
+		// if success
+		if resp.StatusCode == http.StatusOK {
+			userByConds, getErr1 := userRepo.GetByConditions(ctx, map[string]any{
+				"verification_code =": tc.payload.Code,
+			})
+			if getErr1 == nil || userByConds != nil {
+				t.Error("should error and user should nil")
+			}
+
+			userByID, getErr2 := userRepo.GetByID(ctx, userID)
+			if getErr2 == nil || userByID != nil {
+				t.Error("should error and user should nil")
+			}
+		}
+	}
 }
 
 func Test_ForgetPassword(t *testing.T) {
