6ba1b6f93d396fadb42b53ee17002a95efaf7ff8
LukmanE22 <unsurlukman@gmail.com>
2023-11-05 21:46:52 +0700

    refactor app test -2

--- application/app_test.go
+++ application/app_test.go
@@ -7,9 +7,12 @@ package application
 import (
 	"context"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
+	controller "github.com/Lukmanern/gost/controller/development"
 	"github.com/Lukmanern/gost/domain/model"
 	"github.com/Lukmanern/gost/internal/env"
 	"github.com/Lukmanern/gost/internal/helper"
@@ -17,6 +20,7 @@ import (
 	repository "github.com/Lukmanern/gost/repository/user"
 	rbacService "github.com/Lukmanern/gost/service/rbac"
 	service "github.com/Lukmanern/gost/service/user"
+	"github.com/gofiber/fiber/v2"
 )
 
 var (
@@ -59,6 +63,18 @@ func CreateUserAndToken(roleID int) (int, string) {
 	return 0, ""
 }
 
+func Test_RunApp(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			t.Error("should not panic", r)
+		}
+	}()
+
+	go RunApp()
+	time.Sleep(3 * time.Second)
+}
+
 func Test_app_router(t *testing.T) {
 	defer func() {
 		r := recover()
@@ -85,6 +101,64 @@ func Test_app_router(t *testing.T) {
 	if router.Config().ProxyHeader != "" {
 		t.Error("Router's ProxyHeader should be empty")
 	}
-
 	setup()
+}
+
+func TestDevopmentRouter(t *testing.T) {
+	ctr := controller.NewDevControllerImpl()
+	testCases := []struct {
+		endpoint string
+		resCode  int
+		handler  func(c *fiber.Ctx) error
+	}{
+		{"ping/db", http.StatusOK, ctr.PingDatabase},
+		{"ping/redis", http.StatusOK, ctr.PingRedis},
+		{"panic", http.StatusInternalServerError, ctr.Panic},
+		{"storing-to-redis", http.StatusCreated, ctr.StoringToRedis},
+		{"get-from-redis", http.StatusOK, ctr.GetFromRedis},
+	}
+
+	for _, tc := range testCases {
+		app := fiber.New()
+		req := httptest.NewRequest(http.MethodGet, "/development/"+tc.endpoint, nil)
+		app.Get("/development/"+tc.endpoint, tc.handler)
+		res, err := app.Test(req, -1)
+		if err != nil {
+			t.Fatal("should not error")
+		}
+		defer res.Body.Close()
+		if res.StatusCode != tc.resCode {
+			t.Errorf("Expected status code %d, but got %d", tc.resCode, res.StatusCode)
+		}
+	}
+}
+
+func TestRoutes(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			t.Error("should not panic : ", r)
+		}
+	}()
+	router := fiber.New()
+
+	t.Run("getDevelopmentRouter", func(t *testing.T) {
+		getDevopmentRouter(router)
+	})
+
+	t.Run("getRBACAuthRoutes", func(t *testing.T) {
+		getRbacRoutes(router)
+	})
+
+	t.Run("getMiddlewareTestingRoute", func(t *testing.T) {
+		getMiddlewareTestingRoute(router)
+	})
+
+	t.Run("getUserAuthRoutes", func(t *testing.T) {
+		getUserRoutes(router)
+	})
+
+	t.Run("getUserRoutes", func(t *testing.T) {
+		getUserManagementRoutes(router)
+	})
 }
