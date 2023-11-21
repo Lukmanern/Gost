128cfa9386ee9da2991de356992c6446b9e89aee
LukmanE22 <unsurlukman@gmail.com>
2023-11-21 11:46:49 +0700

    refactor development controller test

--- controller/development/dev_controller_test.go
+++ controller/development/dev_controller_test.go
@@ -1,16 +1,24 @@
 package controller
 
 import (
-	"net/http"
+	"encoding/json"
+	"log"
 	"testing"
 
 	"github.com/Lukmanern/gost/database/connector"
+	"github.com/Lukmanern/gost/internal/constants"
 	"github.com/Lukmanern/gost/internal/env"
 	"github.com/Lukmanern/gost/internal/helper"
+	"github.com/Lukmanern/gost/internal/response"
 	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
 )
 
-type handlerF = func(c *fiber.Ctx) error
+const (
+	TestName    = "Development Controller Test"
+	FilePath    = "./controller/development"
+	AddTestName = ", at " + TestName + " in " + FilePath
+)
 
 func init() {
 	// Check env and database
@@ -21,79 +29,172 @@ func init() {
 }
 
 func TestNewDevControllerImpl(t *testing.T) {
+	assert := assert.New(t)
 	ctr := NewDevControllerImpl()
 	c := helper.NewFiberCtx()
-	if ctr == nil || c == nil {
-		t.Error("should not error")
-	}
-
-	pingDbErr := ctr.PingDatabase(c)
-	if pingDbErr != nil {
-		t.Error("err: ", pingDbErr)
-	}
-
-	pingRedisErr := ctr.PingRedis(c)
-	if pingRedisErr != nil {
-		t.Error("err: ", pingRedisErr)
-	}
-
-	panicErr := ctr.Panic(c)
-	if panicErr != nil {
-		t.Error("err: ", panicErr)
-	}
-
-	storingErr := ctr.StoringToRedis(c)
-	if storingErr != nil {
-		t.Error("err: ", storingErr)
-	}
-
-	getErr := ctr.GetFromRedis(c)
-	if getErr != nil {
-		t.Error("err: ", getErr)
-	}
-
-	checkRoleErr := ctr.CheckNewRole(c)
-	if checkRoleErr != nil {
-		t.Error("err: ", checkRoleErr)
-	}
-
-	checkPermErr := ctr.CheckNewPermission(c)
-	if checkPermErr != nil {
-		t.Error("err: ", checkPermErr)
-	}
+	assert.NotNil(ctr, "Expected NewDevControllerImpl to not be nil"+AddTestName)
+	assert.NotNil(c, "Expected FiberCtx to not be nil"+AddTestName)
 }
 
-func TestMethods(t *testing.T) {
-	c := helper.NewFiberCtx()
-	ctr := NewDevControllerImpl()
-	if ctr == nil || c == nil {
-		t.Error("should not nil")
-	}
+type TestCase struct {
+	Name             string
+	Handler          func(*fiber.Ctx) error
+	AdditionalAction func()
+	Payload          map[string]any
+	ResponseCode     int
+	ResponseBody     response.Response
+}
 
-	testCases := []struct {
-		caseName string
-		method   handlerF
-		respCode int
-	}{
-		{"PingDatabase", ctr.PingDatabase, http.StatusOK},
-		{"PingRedis", ctr.PingRedis, http.StatusOK},
-		{"Panic", ctr.Panic, http.StatusInternalServerError},
-		{"StoringToRedis", ctr.StoringToRedis, http.StatusCreated},
-		{"GetFromRedis", ctr.GetFromRedis, http.StatusOK},
-		{"CheckNewRole", ctr.CheckNewRole, http.StatusOK},
-		{"CheckNewPermission", ctr.CheckNewPermission, http.StatusOK},
-		{"UploadFile", ctr.UploadFile, http.StatusBadRequest},
-		{"RemoveFile", ctr.RemoveFile, http.StatusBadRequest},
-		{"GetFilesList", ctr.GetFilesList, http.StatusOK},
+func TestAllControllers(t *testing.T) {
+	assert := assert.New(t)
+	ctr := NewDevControllerImpl()
+	c := helper.NewFiberCtx()
+	assert.NotNil(ctr, "Expected NewDevControllerImpl to not be nil"+AddTestName)
+	assert.NotNil(c, "Expected FiberCtx to not be nil"+AddTestName)
+
+	testCases := []TestCase{
+		{
+			Name:             "PingDB" + AddTestName,
+			Handler:          ctr.PingDatabase,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusOK,
+			ResponseBody: response.Response{
+				Success: true,
+			},
+		},
+		{
+			Name:             "PingRedis" + AddTestName,
+			Handler:          ctr.PingRedis,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusOK,
+			ResponseBody: response.Response{
+				Success: true,
+			},
+		},
+		{
+			Name:             "Panic" + AddTestName,
+			Handler:          ctr.Panic,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusInternalServerError,
+			ResponseBody: response.Response{
+				Success: false,
+			},
+		},
+		{
+			Name:    "GetFromRedis-1" + AddTestName,
+			Handler: ctr.GetFromRedis,
+			AdditionalAction: func() {
+				connector.LoadRedisCache().FlushAll()
+			},
+			ResponseCode: fiber.StatusInternalServerError,
+			ResponseBody: response.Response{
+				Success: false,
+			},
+		},
+		{
+			Name:             "StoringToRedis" + AddTestName,
+			Handler:          ctr.StoringToRedis,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusCreated,
+			ResponseBody: response.Response{
+				Success: true,
+			},
+		},
+		{
+			Name:             "GetFromRedis-2" + AddTestName,
+			Handler:          ctr.GetFromRedis,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusOK,
+			ResponseBody: response.Response{
+				Success: true,
+			},
+		},
+		{
+			Name:    "GetFromRedis-3" + AddTestName,
+			Handler: ctr.GetFromRedis,
+			AdditionalAction: func() {
+				connector.LoadRedisCache().FlushAll()
+			},
+			ResponseCode: fiber.StatusInternalServerError,
+			ResponseBody: response.Response{
+				Success: false,
+			},
+		},
+		{
+			Name:             "CheckNewRole" + AddTestName,
+			Handler:          ctr.CheckNewRole,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusOK,
+			ResponseBody: response.Response{
+				Success: true,
+			},
+		},
+		{
+			Name:             "CheckNewPermission" + AddTestName,
+			Handler:          ctr.CheckNewPermission,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusOK,
+			ResponseBody: response.Response{
+				Success: true,
+			},
+		},
+		{
+			Name:             "UploadFile" + AddTestName,
+			Handler:          ctr.UploadFile,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusBadRequest,
+			ResponseBody: response.Response{
+				Success: false,
+			},
+		},
+		{
+			Name:             "RemoveFile" + AddTestName,
+			Handler:          ctr.RemoveFile,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusBadRequest,
+			ResponseBody: response.Response{
+				Success: false,
+			},
+		},
+		{
+			Name:             "GetFilesList" + AddTestName,
+			Handler:          ctr.GetFilesList,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusOK,
+			ResponseBody: response.Response{
+				Success: true,
+			},
+		},
+		{
+			Name:             "FakeHandler" + AddTestName,
+			Handler:          ctr.FakeHandler,
+			AdditionalAction: nil,
+			ResponseCode:     fiber.StatusOK,
+			ResponseBody: response.Response{
+				Success: true,
+			},
+		},
 	}
 
 	for _, tc := range testCases {
+		log.Println(tc.Name)
 		c := helper.NewFiberCtx()
 		c.Request().Header.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
-		tc.method(c)
-		resp := c.Response()
-		if resp.StatusCode() != tc.respCode {
-			t.Errorf("Expected response code %d, but got %d on: %s", tc.respCode, resp.StatusCode(), tc.caseName)
+		c.Request().Header.SetMethod(fiber.MethodGet)
+
+		if tc.AdditionalAction != nil {
+			tc.AdditionalAction()
 		}
+
+		tc.Handler(c)
+		res := c.Response()
+		assert.Equal(res.StatusCode(), tc.ResponseCode, constants.ShouldEqual, res.StatusCode())
+
+		resBody := res.Body()
+		resString := string(resBody)
+		resBodyStruct := response.Response{}
+		err := json.Unmarshal([]byte(resString), &resBodyStruct)
+		assert.Nilf(err, "failed to parse response JSON: %v", err)
+		assert.Equal(tc.ResponseBody.Success, resBodyStruct.Success, "expected success value should match")
 	}
 }
