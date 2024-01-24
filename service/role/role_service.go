caf0cc302b5604613606503434e876e008e8bda9
LukmanE22 <unsurlukman@gmail.com>
2024-01-24 17:18:33 +0700

    add role service

---
+++ service/role/role_service.go
@@ -0,0 +1,147 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+
+	"github.com/Lukmanern/gost/domain/entity"
+	"github.com/Lukmanern/gost/domain/model"
+	"github.com/Lukmanern/gost/internal/consts"
+	repository "github.com/Lukmanern/gost/repository/role"
+)
+
+type RoleService interface {
+
+	// Create func create one role.
+	Create(ctx context.Context, data model.RoleCreate) (id int, err error)
+
+	// GetByID func get one role.
+	GetByID(ctx context.Context, id int) (role *entity.Role, err error)
+
+	// GetAll func get some roles.
+	GetAll(ctx context.Context, filter model.RequestGetAll) (roles []model.RoleResponse, total int, err error)
+
+	// Update func update one role.
+	Update(ctx context.Context, data model.RoleUpdate) (err error)
+
+	// Delete func delete one role.
+	Delete(ctx context.Context, id int) (err error)
+}
+
+type RoleServiceImpl struct {
+	repository repository.RoleRepository
+}
+
+var (
+	roleServiceImpl     *RoleServiceImpl
+	roleServiceImplOnce sync.Once
+)
+
+func NewRoleService() RoleService {
+	roleServiceImplOnce.Do(func() {
+		roleServiceImpl = &RoleServiceImpl{
+			repository: repository.NewRoleRepository(),
+		}
+	})
+	return roleServiceImpl
+}
+
+func (svc *RoleServiceImpl) Create(ctx context.Context, data model.RoleCreate) (id int, err error) {
+	data.Name = strings.ToLower(data.Name)
+	role, getErr := svc.repository.GetByName(ctx, data.Name)
+	if getErr == nil || role != nil {
+		return 0, fiber.NewError(fiber.StatusBadRequest, "role name has been used")
+	}
+
+	entityRole := entity.Role{
+		Name:        data.Name,
+		Description: data.Description,
+	}
+	entityRole.SetCreateTime()
+	id, err = svc.repository.Create(ctx, entityRole)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func (svc *RoleServiceImpl) GetByID(ctx context.Context, id int) (role *entity.Role, err error) {
+	role, err = svc.repository.GetByID(ctx, id)
+	if err == gorm.ErrRecordNotFound {
+		return nil, fiber.NewError(fiber.StatusNotFound, consts.NotFound)
+	}
+	if err != nil || role == nil {
+		return nil, errors.New("error while getting role data")
+	}
+	return role, nil
+}
+
+func (svc *RoleServiceImpl) GetAll(ctx context.Context, filter model.RequestGetAll) (roles []model.RoleResponse, total int, err error) {
+	roleEntities, total, err := svc.repository.GetAll(ctx, filter)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	roles = []model.RoleResponse{}
+	for _, roleEntity := range roleEntities {
+		newRole := model.RoleResponse{
+			ID:          roleEntity.ID,
+			Name:        roleEntity.Name,
+			Description: roleEntity.Description,
+		}
+		roles = append(roles, newRole)
+	}
+	return roles, total, nil
+}
+
+func (svc *RoleServiceImpl) Update(ctx context.Context, data model.RoleUpdate) (err error) {
+	data.Name = strings.ToLower(data.Name)
+	roleByName, getErr := svc.repository.GetByName(ctx, data.Name)
+	if getErr != nil && getErr != gorm.ErrRecordNotFound {
+		return getErr
+	}
+	if roleByName != nil && roleByName.ID != data.ID {
+		return fiber.NewError(fiber.StatusBadRequest, "role name has been used")
+	}
+
+	role, err := svc.repository.GetByID(ctx, data.ID)
+	if err == gorm.ErrRecordNotFound {
+		return fiber.NewError(fiber.StatusNotFound)
+	}
+	if err != nil || role == nil {
+		return errors.New("error while getting role data")
+	}
+
+	entityRole := entity.Role{
+		ID:          data.ID,
+		Name:        data.Name,
+		Description: data.Description,
+	}
+	entityRole.SetUpdateTime()
+	err = svc.repository.Update(ctx, entityRole)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (svc *RoleServiceImpl) Delete(ctx context.Context, id int) (err error) {
+	role, err := svc.repository.GetByID(ctx, id)
+	if err == gorm.ErrRecordNotFound {
+		return fiber.NewError(fiber.StatusNotFound)
+	}
+	if err != nil || role == nil {
+		return errors.New("error while getting role data")
+	}
+
+	err = svc.repository.Delete(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ service/role/role_service_test.go
@@ -0,0 +1,259 @@
+package service
+
+import (
+	"log"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/Lukmanern/gost/domain/entity"
+	"github.com/Lukmanern/gost/domain/model"
+	"github.com/Lukmanern/gost/internal/consts"
+	"github.com/Lukmanern/gost/internal/env"
+	"github.com/Lukmanern/gost/internal/helper"
+	repository "github.com/Lukmanern/gost/repository/role"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	headerTestName string = "at RoleServiceTest"
+)
+
+var (
+	timeNow        time.Time
+	roleRepository repository.RoleRepository
+)
+
+func init() {
+	envFilePath := "./../../.env"
+	env.ReadConfig(envFilePath)
+	timeNow = time.Now()
+	roleRepository = repository.NewRoleRepository()
+}
+
+// type Role struct {
+// 	ID          int    `gorm:"type:serial;primaryKey" json:"id"`
+// 	Name        string `gorm:"type:varchar(255) not null unique" json:"name"`
+// 	Description string `gorm:"type:varchar(255) not null" json:"description"`
+// 	TimeFields
+//   }
+
+func TestCreate(t *testing.T) {
+	service := NewRoleService()
+	assert.NotNil(t, service, consts.ShouldNotNil, headerTestName)
+	repository := roleRepository
+	assert.NotNil(t, repository, consts.ShouldNotNil, headerTestName)
+	ctx := helper.NewFiberCtx().Context()
+	assert.NotNil(t, ctx, consts.ShouldNotNil, headerTestName)
+
+	role := createRole()
+	defer repository.Delete(ctx, role.ID)
+
+	type testCase struct {
+		Name    string
+		Payload model.RoleCreate
+		WantErr bool
+	}
+
+	testCases := []testCase{
+		{
+			Name: "Success Create Role -1",
+			Payload: model.RoleCreate{
+				Name:        strings.ToLower(helper.RandomString(6)),
+				Description: helper.RandomWords(8),
+			},
+			WantErr: false,
+		},
+		{
+			Name: "Success Create Role -2",
+			Payload: model.RoleCreate{
+				Name:        strings.ToLower(helper.RandomString(6)),
+				Description: helper.RandomWords(8),
+			},
+			WantErr: false,
+		},
+		{
+			Name: "Failed Create Role -2: name has been used",
+			Payload: model.RoleCreate{
+				Name:        role.Name,
+				Description: helper.RandomWords(8),
+			},
+			WantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		log.Println(tc.Name, headerTestName)
+
+		id, err := service.Create(ctx, tc.Payload)
+		if tc.WantErr {
+			assert.Error(t, err, consts.ShouldErr, tc.Name, headerTestName)
+			continue
+		}
+		assert.NoError(t, err, consts.ShouldNotErr, tc.Name, headerTestName)
+
+		// expect no error
+		role, getErr := service.GetByID(ctx, id)
+		assert.NoError(t, getErr, consts.ShouldNotErr, tc.Name, headerTestName)
+		assert.Equal(t, role.Name, tc.Payload.Name, tc.Name, headerTestName)
+		assert.Equal(t, role.Description, tc.Payload.Description, tc.Name, headerTestName)
+
+		deleteErr := service.Delete(ctx, id)
+		assert.NoError(t, deleteErr, consts.ShouldNotErr, tc.Name, headerTestName)
+
+		// expect error
+		_, getErr = service.GetByID(ctx, id)
+		assert.Error(t, getErr, consts.ShouldErr, tc.Name, headerTestName)
+
+		deleteErr = service.Delete(ctx, id)
+		assert.Error(t, deleteErr, consts.ShouldErr, tc.Name, headerTestName)
+
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	service := NewRoleService()
+	assert.NotNil(t, service, consts.ShouldNotNil, headerTestName)
+	repository := roleRepository
+	assert.NotNil(t, repository, consts.ShouldNotNil, headerTestName)
+	ctx := helper.NewFiberCtx().Context()
+	assert.NotNil(t, ctx, consts.ShouldNotNil, headerTestName)
+
+	totalRoleCreated := 5
+
+	for i := 0; i < totalRoleCreated; i++ {
+		role := createRole()
+		defer repository.Delete(ctx, role.ID)
+	}
+
+	type testCase struct {
+		Name    string
+		Payload model.RequestGetAll
+		WantErr bool
+	}
+
+	testCases := []testCase{
+		{
+			Name: "Success Get All Role -1",
+			Payload: model.RequestGetAll{
+				Page:  1,
+				Limit: 100,
+			},
+			WantErr: false,
+		},
+		{
+			Name: "Success Get All Role -2",
+			Payload: model.RequestGetAll{
+				Page:  1,
+				Limit: 10 + totalRoleCreated,
+			},
+			WantErr: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		log.Println(tc.Name, headerTestName)
+
+		roles, total, err := service.GetAll(ctx, tc.Payload)
+		if tc.WantErr {
+			assert.Error(t, err, consts.ShouldErr, tc.Name, headerTestName)
+			continue
+		}
+		assert.NoError(t, err, consts.ShouldNotErr, tc.Name, headerTestName)
+		assert.True(t, len(roles) >= totalRoleCreated, consts.ShouldNotErr, tc.Name, headerTestName)
+		assert.True(t, total >= totalRoleCreated, consts.ShouldNotErr, tc.Name, headerTestName)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	service := NewRoleService()
+	assert.NotNil(t, service, consts.ShouldNotNil, headerTestName)
+	repository := roleRepository
+	assert.NotNil(t, repository, consts.ShouldNotNil, headerTestName)
+	ctx := helper.NewFiberCtx().Context()
+	assert.NotNil(t, ctx, consts.ShouldNotNil, headerTestName)
+
+	role := createRole()
+	defer repository.Delete(ctx, role.ID)
+
+	type testCase struct {
+		Name    string
+		Payload model.RoleUpdate
+		WantErr bool
+	}
+
+	testCases := []testCase{
+		{
+			Name: "Success Update Role -1",
+			Payload: model.RoleUpdate{
+				ID:          role.ID,
+				Name:        strings.ToLower(helper.RandomString(5)),
+				Description: helper.RandomWords(8),
+			},
+			WantErr: false,
+		},
+		{
+			Name: "Success Update Role -2",
+			Payload: model.RoleUpdate{
+				ID:          role.ID,
+				Name:        strings.ToLower(helper.RandomString(5)),
+				Description: helper.RandomWords(8),
+			},
+			WantErr: false,
+		},
+		{
+			Name: "Failed Update Role -1: invalid ID",
+			Payload: model.RoleUpdate{
+				ID: -10,
+			},
+			WantErr: true,
+		},
+		{
+			Name: "Failed Update Role -2: role not found",
+			Payload: model.RoleUpdate{
+				ID: role.ID + 999,
+			},
+			WantErr: true,
+		},
+		{
+			Name: "Failed Update Role -3: name has been used",
+			Payload: model.RoleUpdate{
+				ID:   role.ID,
+				Name: "admin",
+			},
+			WantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		log.Println(tc.Name, headerTestName)
+
+		err := service.Update(ctx, tc.Payload)
+		if tc.WantErr {
+			assert.Error(t, err, consts.ShouldErr, tc.Name, headerTestName)
+			continue
+		}
+		assert.NoError(t, err, consts.ShouldNotErr, tc.Name, headerTestName)
+
+		role, err := service.GetByID(ctx, tc.Payload.ID)
+		assert.NoError(t, err, consts.ShouldNotErr, tc.Name, headerTestName)
+		assert.Equal(t, role.Name, tc.Payload.Name, consts.ShouldNotErr, tc.Name, headerTestName)
+		assert.Equal(t, role.Description, tc.Payload.Description, consts.ShouldNotErr, tc.Name, headerTestName)
+	}
+}
+
+func createRole() entity.Role {
+	repository := roleRepository
+	ctx := helper.NewFiberCtx().Context()
+	role := entity.Role{
+		Name:        strings.ToLower(helper.RandomString(15)),
+		Description: helper.RandomWords(8),
+	}
+	role.SetCreateTime()
+	id, err := repository.Create(ctx, role)
+	if err != nil {
+		log.Fatal("failed create a new user", headerTestName)
+	}
+	role.ID = id
+	return role
+}
